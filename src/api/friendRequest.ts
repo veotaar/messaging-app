4f922ceac063294e71b6c510194f7a7324f4df37
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2024-07-27 23:55:58 +0300

    add mutation function for making friend requests

---
+++ src/api/friendRequest.ts
@@ -0,0 +1,35 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export type friendRequestPayload = {
+  to: string;
+  token: string;
+};
+
+type FriendRequestResponse = {
+  friendRequest: {
+    from: string;
+    to: string;
+    _id: string;
+  };
+};
+
+export const makeFriendRequest = async (friendRequest: friendRequestPayload): Promise<FriendRequestResponse> => {
+  const url = `${BASE_URL}/friend-requests`;
+
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    body: JSON.stringify({ to: friendRequest.to }),
+    headers: {
+      Authorization: friendRequest.token,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+
+  return response.json();
+};
--- src/api/queryOptions.ts
+++ src/api/queryOptions.ts
@@ -1,3 +1,11 @@
-import { QueryClient } from '@tanstack/react-query';
+import { QueryClient, useMutation } from '@tanstack/react-query';
+import { makeFriendRequest } from './friendRequest';
 
 export const queryClient = new QueryClient();
+
+export const useMakeFriendRequestMutation = () => {
+  return useMutation({
+    mutationKey: ['friend-request', 'create'],
+    mutationFn: makeFriendRequest,
+  });
+};
