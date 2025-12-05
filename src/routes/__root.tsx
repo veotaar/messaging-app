83cdc75dc18343feaeb5ba810d425b6cec7e7259
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2025-12-05 21:59:58 +0300

    update tanstack devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import type { AuthContext } from "../lib/auth";
 
 type MyRouterContext = {
